ab3e6654afcecf7f6987fa7a14ab0c8e67a7673f
Maxim Pozhidaev <pogidaevmo@gmail.com>
2016-12-05 14:16:52 +0300

    set gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,128 +1,251 @@
-var gulp       = require('gulp'), // Подключаем Gulp
-	sass         = require('gulp-sass'), //Подключаем Sass пакет,
-	browserSync  = require('browser-sync'), // Подключаем Browser Sync
-	concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-	uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
-	cssnano      = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-	rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
-	del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
-	imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-	pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-	cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
-	spritesmith	 = require('gulp.spritesmith'), //Подключаем сборщик спрайтов
-	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
+var gulp 			= require('gulp'), // require Gulp
+	sass 			= require('gulp-sass'), //require Sass,
+	babel 			= require('gulp-babel'), //require babel (for ES6)
+	browserSync 	= require('browser-sync'), // require Browser Sync
+	concat 			= require('gulp-concat'), // require gulp-concat (for files concatenation)
+	uglify 			= require('gulp-uglifyjs'), // require gulp-uglifyjs (for minification JS)
+	cssnano 		= require('gulp-cssnano'), // require gulp-cssnano (for minification CSS)
+	rename 			= require('gulp-rename'), // require gulp-rename (for changing file names)
+	del 			= require('del'), // require dil (for deleting files)
+	imagemin 		= require('gulp-imagemin'), // require gulp-imagemin (for img optimisation)
+	pngquant 		= require('imagemin-pngquant'), // require imagemin-pngquant (for png optimisation)
+	cache 			= require('gulp-cache'), // require gulp-cache (for cache management)
+	spritesmith 	= require('gulp.spritesmith'), //require gulp.spritesmith (for sprites making)
+	autoprefixer 	= require('gulp-autoprefixer'); // require gulp-autoprefixer (for adding prefixes)
 
-gulp.task('sass', function(){ // Создаем таск Sass
-	setTimeout(function(){
-		return gulp.src('src/sass/**/*.scss') // Берем источник
-			.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-			.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
-			.pipe(gulp.dest('src/css')) // Выгружаем результата в папку src/css
-			.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+
+// -----------------------------------------------------------------------
+// sass
+// -----------------------------------------------------------------------
+
+gulp.task('sass', ['css-libs'], function(){
+	setTimeout(function(){ // gulp falls after @import changing without the statement
+		return gulp.src('src/sass/style.scss')
+			.pipe(sass())
+			// .pipe(uncss({
+		 //        html: ['src/index.html']
+		 //    }))
+			.pipe(autoprefixer([
+				'Android 2.3',
+      			'Android >= 4',
+      			'Chrome >= 20',
+      			'Firefox >= 24',
+      			'Explorer >= 8',
+      			'iOS >= 6',
+      			'Opera >= 12',
+      			'Safari >= 6'], { cascade: true }))
+			.pipe(cssnano())
+			.pipe(rename({suffix: '.min'}))
+			.pipe(gulp.dest('src/css'))
+			.pipe(browserSync.reload({stream: true}))
 		}, 100);
 });
 
-gulp.task('sprite', function(){
-	var spriteData = gulp.src('src/myFiles/sprite/*.png') //путь к картинкам для спрайта
-		.pipe(spritesmith({
-			imgName: 'iconSprite.png',
-			cssName: 'spriteStyle.scss',
-			algorithm: 'binary-tree'
-		}));
-		spriteData.img.pipe(gulp.dest('src/img')); // путь, куда сохраняем картинку
-	spriteData.css.pipe(gulp.dest('src/sass')); // путь, куда сохраняем стили
+
+// -----------------------------------------------------------------------
+// css libs
+// -----------------------------------------------------------------------
+
+gulp.task('css-libs', function(){
+	return gulp.src([
+		'src/libs/fancyBox/source/jquery.fancybox.css' // FancyBox
+		
+
+		// ...
+		])
+		.pipe(concat('libs.min.css'))
+		.pipe(cssnano())
+		.pipe(gulp.dest('src/css'));
 });
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
-	browserSync({ // Выполняем browserSync
-		server: { // Определяем параметры сервера
-			baseDir: 'src' // Директория для сервера - src
+
+
+
+// -----------------------------------------------------------------------
+// js libraries
+// -----------------------------------------------------------------------
+
+gulp.task('libs-js', function() {
+	return gulp.src([
+		'src/libs/jquery/jquery-3.1.1.js' //jQuery
+
+		// ...
+		])
+		.pipe(concat('libs.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('src/js'));
+});
+
+
+
+
+
+
+// -----------------------------------------------------------------------
+// js plugins
+// -----------------------------------------------------------------------
+
+gulp.task('plugins-js', function() {
+	return gulp.src([
+		'src/libs/jcarousel/dist/jquery.jcarousel.min.js', // jCarousel
+		'src/libs/fancyBox/source/jquery.fancybox.js' // FancyBox
+
+
+		// ...
+		])
+		.pipe(concat('plugins.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('src/js'));
+});
+
+
+// -----------------------------------------------------------------------
+// babel
+// -----------------------------------------------------------------------
+
+gulp.task('babel', function(){
+    return gulp.src('src/js/main.es6.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(rename({
+        	basename: "main",
+        	suffix: '.min'
+        }))
+        .pipe(gulp.dest('src/js'))
+});
+
+
+// -----------------------------------------------------------------------
+// browser-sync
+// -----------------------------------------------------------------------
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'src'
 		}
-		// ,
-		// notify: false // Отключаем уведомления
+		// , notify: false // Disable notifications
 	});
 });
 
-gulp.task('scripts', function() {
-	return gulp.src([ // Берем все необходимые библиотеки
-		'src/libs/jquery/dist/jquery.min.js', // Берем jQuery
-		'src/libs/jcarousel/dist/jquery.jcarousel.min.js', // Берем jCarousel
-		'src/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
-		])
-		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
-		.pipe(uglify()) // Сжимаем JS файл
-		.pipe(gulp.dest('src/js')); // Выгружаем в папку src/js
+
+
+
+// -----------------------------------------------------------------------
+// spritesmith
+// -----------------------------------------------------------------------
+
+gulp.task('sprite', function(){
+	var spriteData = gulp.src('src/img/_sprite/*.png')
+		.pipe(spritesmith({
+			imgName: 'spriteImg.png',
+			cssName: 'spriteStyle.scss',
+			algorithm: 'binary-tree'
+		}));
+	spriteData.img.pipe(gulp.dest('src/_sprite')); // sprite dest
+	spriteData.css.pipe(gulp.dest('src/_sprite')); // styles dest
 });
 
 
-// переделать указывать путь руками как в js!!!
-gulp.task('css-libs', function() {
-	return gulp.src([
-	'src/libs/magnific-popup/dist/magnific-popup.css', // Берем Magnific Popup
-	'src/libs/animate.css/animate.min.css' // Берем animate.css
-	]) // Выбираем файл для минификации
-		.pipe(concat('libs.min.css')) //Собираем их в кучу в новом файле libs.min.css
-		.pipe(cssnano()) // Сжимаем
-		.pipe(gulp.dest('src/css')); // Выгружаем в папку src/css
+
+
+// -----------------------------------------------------------------------
+// W A T C H
+// -----------------------------------------------------------------------
+
+gulp.task('watch', ['browser-sync', 'sass', 'libs-js', 'plugins-js', 'babel', 'sprite' ], function() {
+	gulp.watch('src/sass/**/*.scss', ['sass', browserSync.reload]);
+	gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch('src/js/**/*.js', ['babel', browserSync.reload]);
 });
 
-gulp.task('min-css',['css-libs'], function(){
-	return gulp.src('src/css/style.css')
-		.pipe(cssnano()) // Сжимаем
-		.pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-		.pipe(gulp.dest('src/css')); // Выгружаем в папку src/css
-})
 
-gulp.task('watch', ['browser-sync', 'min-css', 'sass', 'scripts', 'sprite' ], function() {
-	gulp.watch('src/sass/**/*.scss', ['sass', browserSync.reload]); // Наблюдение за sass файлами в папке sass
-	gulp.watch('src/css/style.css', ['min-css']); // Наблюдение за css файлами в папке css
-	gulp.watch('src/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-	gulp.watch('src/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
-});
+
+
+
+
+//========================================================================
+
+// - P-R-O-D-U-C-T-I-O-N -
+
+//========================================================================
+
+
+// -----------------------------------------------------------------------
+// clean
+// -----------------------------------------------------------------------
 
 gulp.task('clean', function() {
-	return del.sync('dist'); // Удаляем папку dist перед сборкой
+	return del.sync('dist');
 });
 
+
+
+
+// -----------------------------------------------------------------------
+// img optimisation
+// -----------------------------------------------------------------------
+
 gulp.task('img', function() {
-	return gulp.src('src/img/**/*') // Берем все изображения из src
-		.pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+	return gulp.src('src/img/**/*')
+		.pipe(cache(imagemin({ 
 			interlaced: true,
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
 			use: [pngquant()]
 		})))
-		.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+		.pipe(gulp.dest('dist/img'));
 });
 
 
 
+// -----------------------------------------------------------------------
+// clear cache
+// -----------------------------------------------------------------------
+
+gulp.task('clear', function (callback) {
+	return cache.clearAll();
+})
 
 
-gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+// -----------------------------------------------------------------------
+// B U I L D
+// -----------------------------------------------------------------------
 
-	var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+gulp.task('build', ['clean', 'img', 'sass', 'libs-js', 'plugins-js'], function() {
+
+	// FONTS
+	var buildFonts = gulp.src('src/fonts/**/*') 
+	.pipe(gulp.dest('dist/fonts'))
+
+
+	// CSS
+	var buildCss = gulp.src([ 
 		'src/css/style.min.css',
 		'src/css/libs.min.css'
 		])
 	.pipe(gulp.dest('dist/css'))
 
-	var buildFonts = gulp.src('src/fonts/**/*') // Переносим шрифты в продакшен
-	.pipe(gulp.dest('dist/fonts'))
 
-	var buildJs = gulp.src('src/js/**/*') // Переносим скрипты в продакшен
+	// JavaScript
+	var buildJs = gulp.src(['src/js/**/*.js', '!src/js/main.es6.js'])
 	.pipe(gulp.dest('dist/js'))
 
-	var buildHtml = gulp.src('src/*.html') // Переносим HTML в продакшен
+
+	// HTML
+	var buildHtml = gulp.src('src/*.html')
 	.pipe(gulp.dest('dist'));
 
-	var buildJSON = gulp.src('src/json/*.json') // Переносим josn в продакшен
-	.pipe(gulp.dest('dist'));
+	// JSON
+	var buildJSON = gulp.src('src/json/*.json')
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/json'));
 
 });
 
-gulp.task('clear', function (callback) {
-	return cache.clearAll();
-})
+
+
 
 gulp.task('default', ['watch']);
---
+++ src/js/main.es6.js
@@ -0,0 +1,114 @@
+(function($) {
+
+// search
+$("#search").on("focus", function(){
+	$(".fa-search").css("color", "#279cc7");
+});
+
+$("#search").on("blur", function(){
+	$(".fa-search").css("color", "#828282");
+});
+
+
+$('<i class="fa fa-user" aria-hidden="true"> </i>').prependTo($('.accountMenu')).css('marginRight', 5);
+$('<i class="fa fa-sort-desc" aria-hidden="true"></i>').appendTo($('.selectMenu'));
+
+$(".selectMenu").on("click", function(e){
+	e.preventDefault();
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	var item = e.target;
+	var itemObj = $(e.target.nextElementSibling);
+	function hideMenu(){itemObj.addClass("animated flipOutX").one(
+			animationEnd, function(){$(this).removeClass(
+			'animated flipOutX').addClass("hide");
+			itemObj.css("display", "none");});
+		};
+	if(itemObj.hasClass("hide")){
+		itemObj.css("display", "block");
+		itemObj.addClass("animated flipInX").one(
+			animationEnd, function(){$(this).removeClass(
+			'animated flipInX hide')});
+		itemObj.on('click', function(e){
+			e.preventDefault();
+			var textNode = e.target.innerHTML;
+			item.innerHTML = textNode;
+			hideMenu();
+		});
+	} else{
+		
+		hideMenu();
+	}
+
+})
+
+
+function bigMenuRwsizing() {
+  	var winLen = $('.container').innerWidth();
+  	if( winLen > 768){
+  		$('.featuresMenuSubmenuWrap').width(winLen);
+  		return;
+  	}else if(winLen > 530){
+  		$('.featuresMenuSubmenuWrap').width(400);
+  	return;
+  	}else{
+  		$('.featuresMenuSubmenuWrap').width(280);
+  	}
+}
+bigMenuRwsizing()
+$( window ).resize(bigMenuRwsizing);
+
+
+function makeHover(parentEl, childElem){
+	$(parentEl).on('click', function(e){
+		e.preventDefault();
+	})
+	$(parentEl).hover(
+		function(e){
+			$(childElem).fadeIn();
+			$(parentEl)
+				.closest('div')
+				.children('a')
+				.css('background', '#279cc7')
+		},
+		function(){
+			$(childElem).fadeOut();
+			$(parentEl)
+				.closest('div')
+				.children('a')
+				.css('background', 'none')
+		}
+	);
+}
+
+makeHover('.badroomsMenuItem', '.subMenuBadroomsWrap');
+makeHover('.cartMainDiv', '.cartPopup');
+makeHover('.livingroomMenuItem', '.livingroomMenuSubmenuWrap');
+makeHover('.featuresMainMenuItem', '.featuresMenuSubmenuWrap');
+
+
+
+$(".sandwich, .menu_item").click(function() {
+
+  	$(".sandwich").toggleClass("swich");
+  	menuToggle();
+});
+
+function menuToggle(){
+
+  if($("#sandwich").hasClass('swich')){
+
+  	$('.mainMenuItem').slideDown();
+  	
+
+  } else{
+
+  	$('.mainMenuItem').slideUp();
+}
+
+  }
+
+
+})(jQuery);
+
+
+
