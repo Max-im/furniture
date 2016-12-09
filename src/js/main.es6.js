7124490c71868fca18cfd3418400d1c5d7a79536
Maxim Pozhidaev <pogidaevmo@gmail.com>
2016-12-09 17:18:18 +0300

    finish mainFooter block

--- src/js/main.es6.js
+++ src/js/main.es6.js
@@ -142,11 +142,7 @@ $('#owlCarousel-2').owlCarousel({
 	mouseDrag: false,
 	slideSpeed : 800,
     paginationSpeed : 1300,
-    singleItem: false
-
-    //itemsDesktop : [1000,5], //5 items between 1000px and 901px
-    //itemsDesktopSmall : [900,3], // betweem 900px and 601px
-    //itemsTablet: [600,2], //2 items between 600 and 0
+    singleItem: false,
 
 });
 
